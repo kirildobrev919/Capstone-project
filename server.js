319f25d2972bb8dffd7004a1f9d22b32040c175e
kirildobrev919 <kirildobrev919@gmail.com>
2020-10-06 18:10:16 +0100

    middleware added

---
+++ server.js
@@ -0,0 +1,6 @@
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var errorhandler = require('errorhandler');
+var morgan = require('morgan');
+var express = require('express');
+
