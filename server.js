1dc0ff67c2e73c14163385f2a6cd1fdf5c16fa7f
kirildobrev919 <kirildobrev919@gmail.com>
2020-10-06 19:25:22 +0100

    app in server js setted up port4000

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var errorhandler = require('errorhandler');
-var morgan = require('morgan');
-var express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const morgan = require('morgan');
+const express = require('express');
 
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(errorhandler());
+
+// morgan to log depending on the environment
+if (app.get('env') != 'production') {
+    app.use(morgan('tiny'));
+}
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+})
+
+module.exports = app;
