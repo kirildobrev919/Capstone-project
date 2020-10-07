8846ea552c6681c77150bf2db695f06efc5acde3
kirildobrev919 <kirildobrev919@gmail.com>
2020-10-08 00:22:07 +0100

    working post and not finished put introduced

--- api/artist.js
+++ api/artist.js
@@ -14,28 +14,14 @@ apiArtist.param('artistId', (req, res, next, artistId) => {
             res.sendStatus(404);
         }
     })
-    // const sql = 'SELECT * FROM Artist WHERE id = $artistId';
-    // const values = { $artistId: artistId };
-    // db.get(sql, values, (error, artist) => {
-    //     if (error) {
-    //         next(error);
-    //     } else if (artist) {
-    //         req.artist = artist;
-    //         next();
-    //     } else {
-    //         res.sendStatus(404);
-    //     }
-    // })
 });
 
 apiArtist.get('/', (req, res, next) => {
     db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (err, artists) => {
         if (err) {
             console.log(err);
-            //passing err to middleware errorhandler
             next(err);
         } else {
-            //res sends json object with  artists
             res.status(200).json({ artists: artists });
         }
     })
@@ -45,5 +31,76 @@ apiArtist.get('/:artistId', (req, res, next) => {
     let selectedArtist = req.artist
     res.json({ artist: selectedArtist });
 })
+
+apiArtist.post('/', (req, res, next) => {
+    let artist = req.body.artist;
+    let name = artist.name;
+    let dateOfBirth = artist.dateOfBirth;
+    let biography = artist.biography;
+    let isCurrentlyEmployed = artist.isCurrentlyEmployed === 0 ? 0 : 1;
+
+    if (!req.body.artist) {
+        console.log('first if');
+        res.sendStatus(400);
+    }
+
+    if (!name || !dateOfBirth || !biography || !isCurrentlyEmployed) {
+        console.log('sec if');
+        res.sendStatus(400);
+    }
+
+    let sql = 'INSERT INTO Artist (name, date_of_birth, biography, is_currently_employed) VALUES ($name, $dob, $biography, $ice)';
+    let values = {
+        $name: name,
+        $dob: dateOfBirth,
+        $biography: biography,
+        $ice: isCurrentlyEmployed
+    }
+    db.run(sql, values, function (error) {
+        if (error) {
+            console.log('after insert in err');
+            next(err);
+        }
+        db.get(`SELECT * FROM Artist WHERE id = ${this.lastID}`, (err, artist) => {
+            if (err) {
+                return res.sendStatus(500);
+            } else {
+                res.status(201).json({ artist: artist });
+            }
+        });
+    })
+})
+
+apiArtist.put('/:artistId', (req, res, next) => {
+    const newArtist = req.body.artist;
+
+    if (!newArtist.name || !newArtist.dateOfBirth || !newArtist.biography || !newArtist.isCurrentlyEmployed) {
+        res.sendStatus(400);
+    }
+
+    let values = {
+        $name: artist.name,
+        $dob: artist.dateOfBirth,
+        $biography: artist.biography,
+        $ice: artist.isCurrentlyEmployed,
+        $id: artist.id
+    }
+    let sql = `UPDATE TABLE Artist SET (name = $name, date_of_birth = $dob, biography = $biography,` +
+        `is_currently_employed = $ice WHERE id = $id)`;
+    db.run(sql, values, (error) => {
+        if (error) {
+            next(error);
+        }
+        db.get(`SELECT * FROM Artist WERE id = ${this.lastID}`, (err, artist) => {
+            if (err) {
+                res.sendStatus(400);
+            } else {
+                res.status(200).json({ artist: artist });
+            }
+        })
+    })
+})
+
+//})
 
 module.exports = apiArtist;
